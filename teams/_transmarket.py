f0aa7330b056b2992c3cb7a886a7228344bbbd5a
ignaciodpaez <ignpaez@yahoo.com>
2026-02-13 20:06:53 +0000

    Add get competition clubs

--- teams/_transmarket.py
+++ teams/_transmarket.py
@@ -1,9 +1,25 @@
+import concurrent.futures as cf
 import json
 import os
 import urllib.parse
 
 import requests
 import pandas as pd
+
+
+competition_map = {
+    "GB1": "Premier League",
+    "ES1": "La Liga",
+    "IT1": "Serie A",
+    "FR1": "Ligue 1",
+    "L1": "Bundesliga",
+    "PO1": "Primeira Liga",
+    "NL1": "Eredivisie",
+    "BRA1": "Campeonato Brasileiro Série A",
+    "ARG1": "Primera División Argentina",
+    # "CL": "Champions League",
+    # "EL": "Europa League",
+}
 
 
 class TransfermarktGateway:
@@ -66,38 +82,20 @@ class TransfermarktGateway:
         
         return data
 
-    def fetch_competition_clubs(self, competition_id, season_id=None, save_to_file=False):
-        """
-        Get the clubs for a given competition and season.
-        If the data is already cached in a file, it will be loaded from there.
-        Otherwise, it will be fetched from the API. Optionally data can 
-        be saved to a file.
-        """
-        url = self.build_competition_clubs_url(competition_id, season_id)
-        file_name = (
-            urllib.parse.quote(url, safe="").replace("/", "_") + ".json"
-        )
-        file_name = f"data/{file_name}"
-        try:
-            with open(file_name, "r") as file:
-                data = json.load(file)
-        except FileNotFoundError:
-            print(f"File {file_name} not found. Fetching data from API.")
-            data = self.fetch_competition_clubs_data(competition_id, season_id)
-            if save_to_file:
-                with open(file_name, "w") as file:
-                    json.dump(data, file, indent=4)
+    def get_competition_clubs(self, competition_id, season_id=None, save_to_file=False):
+        data = self.fetch_competition_clubs(competition_id, season_id)
+        data = pd.DataFrame(data)
 
         return data
     
-    def fetch_players_async(self, club_id, save_to_file=False):
-        import concurrent.futures as cf
+    def get_players_async(self, club_id, seasons=None, save_to_file=False):
 
-        params = [(club_id, i, save_to_file) for i in range(1990, 2026)]
+        years = range(1990, 1993) if seasons is None else seasons
+        params = [(club_id, i, save_to_file) for i in years]
         results = []
 
         with cf.ThreadPoolExecutor(max_workers=10) as executor:
-            future_to_url = {executor.submit(self.fetch_players, *param): param for param in params}
+            future_to_url = {executor.submit(self.get_players, *param): param for param in params}
             for future in cf.as_completed(future_to_url):
                 param = future_to_url[future]
                 try:
@@ -111,13 +109,4 @@ class TransfermarktGateway:
         return results
     
     def delete_players_file(self, club_id, season_id):
-        url = self.build_players_url(club_id, season_id)
-        file_name = (
-            urllib.parse.quote(url, safe="").replace("/", "_") + ".json"
-        )
-        file_name = f"data/{file_name}"
-        try:
-            os.remove(file_name)
-            print(f"File {file_name} deleted.")
-        except FileNotFoundError:
-            print(f"File {file_name} not found.")
+        pass
